499845d3743478eb5d609f8ccacfa4ac026aeb81
Nick Wu <nickwu78@gmail.com>
2017-03-23 21:33:30 +1100

    Added user registration and login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321121824) do
+ActiveRecord::Schema.define(version: 20170323094613) do
 
   create_table "abilities", force: :cascade do |t|
     t.integer "gig_id"
@@ -33,7 +33,9 @@ ActiveRecord::Schema.define(version: 20170321121824) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.integer  "category_id"
+    t.integer  "user_id"
     t.index ["category_id"], name: "index_gigs_on_category_id"
+    t.index ["user_id"], name: "index_gigs_on_user_id"
   end
 
   create_table "proposals", force: :cascade do |t|
@@ -42,11 +44,30 @@ ActiveRecord::Schema.define(version: 20170321121824) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "gig_id"
+    t.integer  "user_id"
     t.index ["gig_id"], name: "index_proposals_on_gig_id"
+    t.index ["user_id"], name: "index_proposals_on_user_id"
   end
 
   create_table "skills", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
