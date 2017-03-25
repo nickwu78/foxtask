3ed035fce0da0f0ca5e2fc1385ef08ad911455c6
Nick Wu <nickwu78@gmail.com>
2017-03-25 21:08:31 +1100

    Made front page list all the gigs

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,9 +1,30 @@
 class GigsController < ApplicationController
 
 def index
+  @gigs = Gig.all.order("created_at DESC")
 end
 
 def new
+  @gig = Gig.new
+end
+
+def create
+  @gig = current_user.gigs.new(gig_params)
+  if @gig.save
+    redirect_to @gig, notice: "Job created"
+  else
+    render :new
+  end
+end
+
+def show
+  @gig = Gig.find(params[:id])
+end
+
+private
+
+def gig_params
+  params.require(:gig).permit(:name, :description, :budget, :location, :open)
 end
 
 end
--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,7 +1,7 @@
 class Gig < ActiveRecord::Base
   has_many :proposals
   belongs_to :user
-  belong_to :category
+  # belongs_to :category
   has_many :abilities
   has_many :skills, through: :abilities
 end
