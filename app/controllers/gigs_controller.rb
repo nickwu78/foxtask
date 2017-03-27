cb4c58aa85bc565d6ae178c11fb7728f41e790e1
Nick Wu <nickwu78@gmail.com>
2017-03-28 00:39:47 +1100

    Implement search

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -1,4 +1,5 @@
 class GigsController < ApplicationController
+before_action :set_gig, only: [:show, :edit, :update, :delete]
 
 def index
   @gigs = Gig.all.order("created_at DESC").page(params[:page]).per(15)
@@ -18,13 +19,20 @@ def create
 end
 
 def show
-  @gig = Gig.find(params[:id])
+
+end
+
+def search
+  @gigs = Gig.search(params).page(params[:page]).per(25)
 end
 
 private
 
-def gig_params
-  params.require(:gig).permit(:name, :description, :budget, :location, :open)
-end
+  def gig_params
+    params.require(:gig).permit(:name, :description, :budget, :location, :open, :user_id, :category_id)
+  end
 
+  def set_gig
+    @gig = Gig.find(params[:id])
+  end
 end
